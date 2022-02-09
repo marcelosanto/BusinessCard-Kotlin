90bca8c04321a3cd90711b1d70adee1d4cbf45c3
Marcelo Santos <rocha.mer21@gmail.com>
2022-02-09 08:19:38 -0300

    criado o adapter

--- app/src/main/java/xyz/com/marcelo/bussinescard/ui/MainActivity.kt
+++ app/src/main/java/xyz/com/marcelo/bussinescard/ui/MainActivity.kt
@@ -2,24 +2,40 @@ package xyz.com.marcelo.bussinescard.ui
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import xyz.com.marcelo.bussinescard.App
 import xyz.com.marcelo.bussinescard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
+
+    private val adapter by lazy { BusinessCardAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        binding.rvCards.adapter = adapter
+        getAllBusinessCard()
         insertListerner()
     }
 
-    fun insertListerner() {
+    private fun insertListerner() {
         binding.fab.setOnClickListener {
             val intent = Intent(this@MainActivity, AddBusinessCardActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun getAllBusinessCard() {
+        mainViewModel.getAll().observe(this) { businessCard ->
+            adapter.submitList(businessCard)
         }
     }
 }
