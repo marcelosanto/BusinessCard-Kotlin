2f8460175e3bd355f754352d3319e4bdf26c0f4c
Marcelo Santos <rocha.mer21@gmail.com>
2022-02-08 22:27:00 -0300

    add a recyclerview no main principal

--- app/src/main/java/xyz/com/marcelo/bussinescard/ui/MainActivity.kt
+++ app/src/main/java/xyz/com/marcelo/bussinescard/ui/MainActivity.kt
@@ -3,10 +3,15 @@ package xyz.com.marcelo.bussinescard.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import xyz.com.marcelo.bussinescard.R
+import xyz.com.marcelo.bussinescard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
